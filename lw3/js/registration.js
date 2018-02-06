62f20b2245c8240516c8ccae877e397e56d20324
Rechkanet <vilyge1232@gmail.com>
2018-02-06 16:32:52 +0300

    Переписал функцию checkValidEmail

--- lw3/js/registration.js
+++ lw3/js/registration.js
@@ -1,40 +1,8 @@
 function checkValidEmail() {
-  var email = document.getElementsByClassName("registration__email")[0].value;
-  var result = email.match(/^[0-9a-z-\.]+\@[0-9a-z-]{1,}\.[a-z]{2,}$/i);
+  var email = $(".registration__email");
+  var result = $(email[0]).val().match(/^[0-9a-z-\.]+\@[0-9a-z-]{1,}\.[a-z]{2,}$/i);
   if (!result){
     return false;
   }
   return true;
 }
-
-function checkPasswords() {
-  var pass = document.getElementsByClassName("registration__password")[0].value; 
-  var passConfirm = document.getElementsByClassName("registration__password-repeat")[0].value;
-  if ((pass.length < 6) || (pass != passConfirm)) {
-    return false;  
-  }
-  return true;
-}
-
-function checkRules() {
-  var result = document.getElementsByClassName("registration__check-confirmation")[0].checked;
-  if (!result) {
-    return false;
-  }
-  return true;
-}
-
-document.getElementsByClassName("registration")[0].onsubmit = function() {
-  if (checkValidEmail() && checkPasswords() && checkRules()) {
-    alert("Регистрация прошла успешно!");  
-  } else {
-    if (!checkValidEmail()) {
-      alert('Введите валидный e-mail.');
-    } else if (!checkPasswords()) {
-      alert('Пароли должны быть неменее 6-ти символов и совпадать.');
-    } else {
-      alert('Примите условия соглашения.');
-    }
-    return false;
-  }
-};
