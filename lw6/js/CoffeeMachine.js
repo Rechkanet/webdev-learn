ab7218b4706d49bfabb681a53d95df73e9e91169
Rechkanet <vilyge1232@gmail.com>
2018-02-15 22:12:32 +0300

    Создал методы setCash и checkCash

--- lw6/js/CoffeeMachine.js
+++ lw6/js/CoffeeMachine.js
@@ -8,4 +8,17 @@ class CoffeeMachine {
     ];
     this.coffeeNumber = null;
   }
+
+  setCash(cash) {
+    if (this.checkCash(cash)) {
+      this.balance += cash;
+      return true;
+    }
+    return false;
+  }
+
+  checkCash(cash) {
+    return cash === 1 || cash === 2 || cash === 5 || 
+           cash === 10 || cash === 50 || cash === 100;
+  }
 }
