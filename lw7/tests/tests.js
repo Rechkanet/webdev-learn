be3742b7fe9e9d060eabb43776af4df293aa5cc5
Rechkanet <vilyge1232@gmail.com>
2018-02-16 08:33:05 +0300

    Создал тест метода checkCash

--- lw7/tests/tests.js
+++ lw7/tests/tests.js
@@ -19,3 +19,35 @@ describe('Метод getCoffeeMenu', function() {
     });
   });
 });
+
+describe('Метод checkCash', function() {
+  function makeTest(value, expected) {
+    it('принимает на вход ' + value + ' и должен вернуть ' + expected, function() {
+      let testCoffeMachine = new CoffeeMachine();
+      let actual = testCoffeMachine.checkCash(value);
+      assert.equal(expected, actual);
+    });
+  }
+  let arranges = [
+    { value: null, expected: false },
+    { value: undefined, expected: false },
+    { value: -1, expected: false },
+    { value: -10, expected: false },
+    { value: 0, expected: false },
+    { value: 11, expected: false },
+    { value: 6, expected: false },
+    { value: 15, expected: false },
+    { value: 13, expected: false },
+    { value: 9, expected: false },
+    { value: 1, expected: true },
+    { value: 2, expected: true },
+    { value: 5, expected: true },
+    { value: 10, expected: true },
+    { value: 50, expected: true },
+    { value: 100, expected: true }
+  ];
+  arranges.forEach(function(arrange, i) {
+    makeTest(arrange.value, arrange.expected);
+  });
+});
+
