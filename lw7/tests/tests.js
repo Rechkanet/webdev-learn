c027525e83e7408764906258cd0f360de6f4bcf0
Rechkanet <vilyge1232@gmail.com>
2018-02-16 08:45:56 +0300

    Создал тест метода checkCoffeeNumber

--- lw7/tests/tests.js
+++ lw7/tests/tests.js
@@ -6,7 +6,7 @@ const assert = require('chai').assert;
 describe('Конструктор без параметров', function() {
   it(`Cоздает новый объект, ошибок не ожидается`, function() {
     assert.doesNotThrow(function() {
-      let testCoffeMachine = new CoffeeMachine();
+      let test = new CoffeeMachine();
     });
   });
 });
@@ -14,17 +14,17 @@ describe('Конструктор без параметров', function() {
 describe('Метод getCoffeeMenu', function() {
   it(`Выводить в консоли кофе меню, ошибок не ожидается`, function() {
     assert.doesNotThrow(function() {
-      let testCoffeMachine = new CoffeeMachine();
-      testCoffeMachine.getCoffeeMenu();
+      let test = new CoffeeMachine();
+      test.getCoffeeMenu();
     });
   });
 });
 
 describe('Метод checkCash', function() {
   function makeTest(value, expected) {
-    it(`принимает ${value} и должен вернуть ${expected}`, function() {
-      let testCoffeMachine = new CoffeeMachine();
-      let actual = testCoffeMachine.checkCash(value);
+    it(`принимает на вход ${value} и должен вернуть ${expected}`, function() {
+      let test = new CoffeeMachine();
+      let actual = test.checkCash(value);
       assert.equal(expected, actual);
     });
   }
@@ -54,10 +54,10 @@ describe('Метод checkCash', function() {
 describe('Метод setCash', function() {
   function makeTest(value, expected) {
     it(`добавляет ${value.cash} к текущему балансу ${value.balance}, баланс должен быть: ${expected}`, function() {
-      let testCoffeMachine = new CoffeeMachine();
-      testCoffeMachine.currentBalance = value.balance;
-      testCoffeMachine.setCash(value.cash);
-      let actual = testCoffeMachine.currentBalance;
+      let test = new CoffeeMachine();
+      test.currentBalance = value.balance;
+      test.setCash(value.cash);
+      let actual = test.currentBalance;
       assert.equal(expected, actual);
     });
   }
@@ -73,6 +73,29 @@ describe('Метод setCash', function() {
     { value: { balance: 0, cash: 51 }, expected: 0 },
     { value: { balance: 5, cash: 51 }, expected: 5 },
     { value: { balance: 10, cash: 51 }, expected: 10 },
+  ];
+  arranges.forEach(function(arrange, i) {
+    makeTest(arrange.value, arrange.expected);
+  });
+});
+
+describe('Метод checkCoffeeNumber', function() {
+  function makeTest(value, expected) {
+    it(`принимает на вход ${value} и должен вернуть ${expected}`, function() {
+      let test = new CoffeeMachine();
+      let actual = test.checkCoffeeNumber(value);
+      assert.equal(expected, actual);
+    });
+  }
+  let arranges = [
+    { value: null, expected: false },
+    { value: undefined, expected: false },
+    { value: -1, expected: false },
+    { value: 4, expected: false },
+    { value: 0, expected: false },
+    { value: 1, expected: true },
+    { value: 2, expected: true },
+    { value: 3, expected: true }
   ];
   arranges.forEach(function(arrange, i) {
     makeTest(arrange.value, arrange.expected);
