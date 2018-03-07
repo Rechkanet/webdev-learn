d22ad62d1afcbe9d476738db2c3b303451887cbd
rechkanet <vilyge1232@gmail.com>
2018-03-07 14:30:10 +0300

    Создал страницу отзывов

--- lw9/src/containers/ReviewsPage/ReviewsPage.js
+++ lw9/src/containers/ReviewsPage/ReviewsPage.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Badge } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
-import './MainPage.css';
+import './ReviewsPage.css';
 import { ERROR_404, ERROR_500 } from '../../constants/Errors';
 
-class MainPage extends Component {
+class ReviewsPage extends Component {
   constructor(props) {
     super(props);
+    this.productId = this.props.match.params.id;
     this.state = {
       error: null,
       isLoaded: false,
-      products: []
+      reviews: []
     };
   }
 
   componentDidMount() {
-    fetch("http://localhost:1337/api/products")
+    fetch(`http://localhost:1337/api/products/${this.productId}/reviews`)
       .then(res => res.json())
       .then(
         (result) => {
           this.setState({
             isLoaded: true,
-            products: result
+            reviews: result
           });
         },
         (error) => {
@@ -32,27 +33,26 @@ class MainPage extends Component {
         }
       )
   }
+
   render() {
-    const { error, isLoaded, products } = this.state;
+    const { error, isLoaded, reviews } = this.state;
     if (error) {
       return <h1><Badge color="danger">Error: {error.message}</Badge></h1>;
     } else if (!isLoaded) {
       return <h1><Badge color="info">Loading...</Badge></h1>;
-    } else if (JSON.stringify(products) === ERROR_404) {
+    } else if (JSON.stringify(reviews) === ERROR_404) {
       return <h1><Badge color="warning">Not found 404!</Badge></h1>;
-    } else if (JSON.stringify(products) === ERROR_500) {
+    } else if (JSON.stringify(reviews) === ERROR_500) {
       return <h1><Badge color="danger">Error 500!</Badge></h1>;
     } else {
       return (
         <Container>
-          <h1><Badge color="success">Products:</Badge></h1>
+          <h1><Badge color="success">Reviews:</Badge></h1>
           <ListGroup>
-            {products.map(product => (
-              <NavLink className='product' to={`/reviews/${product._id}`}>
-                <ListGroupItem className="text" key={product._id}>
-                  {product.name}
-                </ListGroupItem>
-              </NavLink>
+            {reviews.map(review => (
+              <ListGroupItem className="text" key={review._id}>
+                {review.discription}
+              </ListGroupItem>
             ))}
           </ListGroup>
         </Container>
@@ -61,4 +61,4 @@ class MainPage extends Component {
   }
 }
 
-export default MainPage;
+export default ReviewsPage;
--- lw9/src/index.js
+++ lw9/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './containers/App/App';
+import Reviews from './containers/ReviewsPage/ReviewsPage';
 import { BrowserRouter , Route, Switch } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -10,6 +11,7 @@ ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route exact path='/' component={App}/>
+      <Route exact path='/reviews/:id' component={Reviews}/>
     </Switch>
   </BrowserRouter>, 
   document.getElementById('root')
